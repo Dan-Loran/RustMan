4a699bb3e4451bf0634a171205f44e85abe662a4
Dan-Loran <dan@danloran.com>
2026-03-18 16:43:44 -0600

    Delete RustMan.Presentation directory

--- RustMan.Presentation/Modules/ConsoleStream/Actions/ConsoleStreamInterfaceActions.cs
+++
@@ -1,9 +0,0 @@
-namespace RustMan.Presentation.Modules.ConsoleStream.Actions;
-
-public sealed class ConsoleStreamInterfaceActions
-{
-    public Task ClearAsync(CancellationToken cancellationToken = default)
-    {
-        return Task.CompletedTask;
-    }
-}
